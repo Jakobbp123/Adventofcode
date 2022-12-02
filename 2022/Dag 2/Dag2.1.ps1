eb990e88fec2f22dbfabcd035898e8916ba9701d
Jakobbp123 <Jakob.nordvoll@hotmail.com>
2022-12-02 14:17:08 +0100

    topkek

---
+++ 2022/Dag 2/Dag2.1.ps1
@@ -0,0 +1,14 @@
+##[array]$r = (Get-content .\input.txt)
+
+$possibleHands = @('X','Y','Z')
+$translatedHands = @('Rock', 'Paper', 'Scisor')
+$numValue = @(1,2,3)
+
+for($i=0;$i -lt $n;$i++){
+    for($j=0;$j -lt $possibleHands.count;$j++){
+
+
+    }
+
+
+}
